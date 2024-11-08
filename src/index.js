8159edd33237e03d119b40311bf96b9d0fd9e0f0
kentangkiting <agustinus_bintang@yahoo.com>
2024-11-08 14:06:36 +0700

    homework29_2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import Product from "./Product";
+import ListMenu from "./ListMenu";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
   <StrictMode>
-    <Product isLoggedIn={true} />
+    <span>=============homework29_1============</span>
+    <Product />
+    <span>=============homework29_2============</span>
+    <ListMenu />
+    <span>=============homework29_3============</span>
   </StrictMode>
 );
